438449ce20fdbbf9b6d26bcb7650b78e6a5884c7
Karivool <themataaa@gmail.com>
2017-01-20 06:29:18 -0500

    Add constants for sounds

---
+++ constants/sounds.js
@@ -0,0 +1,11 @@
+const SoundFX = [
+  "./sounds/slide01.mp3",
+  "./sounds/slide02.mp3",
+  "./sounds/slide03.mp3",
+  "./sounds/slide04.mp3",
+  "./sounds/slide05.mp3",
+  "./sounds/slide06.mp3",
+  "./sounds/slide07.mp3",
+]
+
+module.exports = SoundFX;
